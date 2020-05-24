cf745908efc20aea0a126cfa1fda592f33242b05
AlanDiasdaSilva <62970087+AlanDiasdaSilva@users.noreply.github.com>
2020-05-23 22:55:13 -0300

    Comando Olá mundo
    Comando Olá mundo

---
+++ Arquivo.java
@@ -0,0 +1,15 @@
+Comando para apresentar mensagem Olá mundo na Tela
+
+------------------------------------------------------------------
+
+package primeiroprograma;
+
+public class Primeiroprograma {
+    
+public static void main (String [] args ){
+    System.out.print("Olá mundo");
+    
+} 
+    
+}
+--------------------------------------------------------------------
